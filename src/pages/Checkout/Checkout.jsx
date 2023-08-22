b551e8eace28ec582b05af6bf704758262d68da9
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2023-08-22 20:12:58 -0300

    checkout enhancenment

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Wallet, initMercadoPago } from "@mercadopago/sdk-react";
-import { getUserAction } from "../../redux/actions";
 import EmptyCart from "../empty/emptyCart";
 import RemoveCircleTwoToneIcon from "@mui/icons-material/RemoveCircleTwoTone";
-import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Checkout.css";
 
@@ -12,40 +10,32 @@ initMercadoPago("APP_USR-8c926d78-0d84-43b8-a918-9da21227b3a9", {
 });
 
 export default function Checkout() {
-  const { user } = useAuth0();
   const dispatch = useDispatch();
-  const email = user?.email;
-  const buyer = useSelector((state) => state.user);
-  const products = useSelector((state) => state.checkUser.cart);
-  const prodToBuy = products;
+  const buyer = useSelector((state) => state.userCheck);
+  const products = useSelector((state) => state.cart);
   const [quantity, setQuantity] = useState(1);
 
-  useEffect(() => {
-    dispatch(getUserAction(email));
-  }, [dispatch, email, products, quantity]);
-
-  const getTotalPrice = () => {
-    let totalPrice = 0;
-    prodToBuy.forEach((item) => {
-      totalPrice += item.price;
-    });
-    return totalPrice.toFixed(2);
-  };
-  const [price, setPrice] = useState(getTotalPrice());
-
-  const deleteProd = (id) => {
-    let arr = products.filter((prod) => prod._id !== id);
-    localStorage.setItem("cartList", JSON.stringify(arr));
-    setProducts(arr);
+  const handleDeleteCartItem = (itemId) => {
+    const userId = buyer?._id;
+    dispatch(deleteCartItemAction(userId, itemId));
   };
 
   if (!products || products.length === 0) {
     return <EmptyCart />;
   }
 
+  const getTotalPrice = () => {
+    let totalPrice = 0;
+    products.forEach((item) => {
+      totalPrice += item.price;
+    });
+    return totalPrice.toFixed(2);
+  };
+  const [price, setPrice] = useState(getTotalPrice());
+
   const onSubmit = async (formData) => {
     const preferenceData = {
-      items: prodToBuy,
+      items: products,
       purpose: "wallet_purchase",
     };
 
@@ -79,38 +69,46 @@ export default function Checkout() {
   };
 
   return (
-    <div className="checkout-container">
-      <div className="checkout-header">
+    <div className='checkout-container'>
+      <div className='checkout-header'>
         <h1>Concreta tu Compra!</h1>
       </div>
 
-      <div className="checkout-payment">
+      <div className='checkout-payment'>
         <h2>Alternativas de Pago</h2>
       </div>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "calc(100% - 315px)",
+          height: "120px",
+          position: "sticky",
+        }}
+      >
         <Wallet onSubmit={onSubmit} onReady={onReady} onError={onError} />
       </div>
 
-      <div className="checkout-product-list">
+      <div className='checkout-product-list'>
         <h3>Listado de Producto/s</h3>
         <br />
 
-        {prodToBuy?.map((item) => (
-          <div key={item.product} className="product-item">
-            <div className="product-image">
-              <img src={item.image} alt={item.name} loading="lazy" />
+        {products?.map((item) => (
+          <div key={item.product} className='product-item'>
+            <div className='product-image'>
+              <img src={item.image} alt={item.name} loading='lazy' />
             </div>
 
-            <div className="product-details">
-              <div className="product-info">
-                <h4 className="product-name">{item.name}</h4>
-                <h4 className="product-price">${item.price}</h4>
-                //<h4 className="product-brand">{item.color}</h4>
+            <div className='product-details'>
+              <div className='product-info'>
+                <h4 className='product-name'>{item.name}</h4>
+                <h4 className='product-price'>${item.price}</h4>
+                <h4 className='product-brand'>{item.color}</h4>
               </div>
 
-              <div className="product-quantity">
+              <div className='product-quantity'>
                 <span>Cantidad: {item.quantity}</span>
-                <button onClick={() => deleteProd(item._id)}>
+                <button onClick={() => handleDeleteCartItem(item._id)}>
                   <RemoveCircleTwoToneIcon />
                 </button>
               </div>
@@ -119,7 +117,7 @@ export default function Checkout() {
         ))}
       </div>
 
-      <div className="checkout-total">
+      <div className='checkout-total'>
         <h4>Precio total: ${price}</h4>
       </div>
       <br />
