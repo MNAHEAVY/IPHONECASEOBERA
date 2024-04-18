81dea964ebd94c1ca3155a7eea77defbd3cbb260
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2024-04-18 19:21:10 -0300

    suscription post

--- src/pages/Obercoins/Suscription.jsx
+++ src/pages/Obercoins/Suscription.jsx
@@ -2,47 +2,50 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Loading from "../Loading/Loading";
 import vid from "../../assets/logo.png";
+import { useSearchParams } from "react-router-dom";
 
 export default function Suscription() {
   const [loading, setLoading] = useState(true);
   const buyer = useSelector((state) => state.checkUser);
+  const [searchParams] = useSearchParams();
+  const parametro = searchParams.get("parametro");
 
-  // useEffect(() => {
-  //   const sendSuscriptionData = async () => {
-  //     const buyerData = {
-  //       payer: buyer,
-  //     };
-  //     try {
-  //       const response = await fetch(
-  //         "https://iphonecaseoberab-production.up.railway.app/suscription",
-  //         {
-  //           method: "POST",
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //           },
-  //           body: JSON.stringify(buyerData),
-  //         }
-  //       );
-  //       const preference = await response.json();
-  //       console.log("Preference ID:", preference.preferenceId);
-  //     } catch (error) {
-  //       console.error(error);
-  //       throw new Error("Error al enviar los datos");
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
+  console.log(parametro);
 
-  //   if (buyer) {
-  //     sendSuscriptionData(); // Call the function when the component mounts
-  //   } else {
-  //     setLoading(false); // Set loading to false if there's no data to send
-  //   }
-  // }, [buyer]);
+  useEffect(() => {
+    const sendSubscriptionData = async () => {
+      const buyerData = {
+        payer: buyer,
+        parametro: parametro, // Incluye el parámetro del path en el objeto de datos
+      };
+      try {
+        const response = await fetch("http://localhost:3001/suscription", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(buyerData),
+        });
+        const responseData = await response.json();
+        console.log("Response Data:", responseData);
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error al enviar los datos");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  // if (loading) {
-  //   return <Loading />; // You can replace this with your loading indicator
-  // }
+    if (buyer) {
+      sendSubscriptionData();
+    } else {
+      setLoading(false);
+    }
+  }, [buyer, parametro]);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <div className='containA'>
