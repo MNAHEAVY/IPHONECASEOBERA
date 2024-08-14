38e4497f018e7778f80466a538ceb2c4272dab32
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2024-08-14 19:01:59 -0300

    iphonecaseobera v2.0.0

--- src/pages/Loading/Loading.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Loading.css";
-
-const Loading = () => {
-  return <div class="loading">Loading&#8230;</div>;
-};
-
-export default Loading;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import appReducer from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import productsSlice from "./reducers/productsSlice";
+import authSlice from "./reducers/authSlice";
+import userSlice from "./reducers/userSlice";
+import cartSlice from "./reducers/cartSlice";
+import valuesSlice from "./reducers/valuesSlice";
 
 const store = configureStore({
-  reducer: appReducer,
-  middleware: [...getDefaultMiddleware(), thunk],
+  reducer: {
+    products: productsSlice,
+    auth: authSlice,
+    user: userSlice,
+    cart: cartSlice,
+    values: valuesSlice,
+  },
 });
 
 export default store;
