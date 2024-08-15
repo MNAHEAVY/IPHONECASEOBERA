bff64f2a4520c62809744277ea5eab6ea834c02c
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2024-08-15 15:03:45 -0300

    cart&fav update

--- src/Views/Cart&Fav/Cart&Fav.jsx
+++ src/Views/Cart&Fav/Cart&Fav.jsx
@@ -50,7 +50,7 @@ export default function CartFav({ type, onClose }) {
                 <div className='flex-1 overflow-y-auto px-4 py-6 sm:px-6'>
                   <div className='flex items-start justify-between'>
                     <DialogTitle className='text-lg font-medium text-gray-900'>
-                      {type === "cart" ? "Your Cart" : "Your Favorites"}
+                      {type === "cart" ? "Tu Carrito" : "Tus Favoritos"}
                     </DialogTitle>
                     <div className='ml-3 flex h-7 items-center'>
                       <button
@@ -59,7 +59,7 @@ export default function CartFav({ type, onClose }) {
                         className='relative -m-2 p-2 text-gray-400 hover:text-gray-500'
                       >
                         <span className='absolute -inset-0.5' />
-                        <span className='sr-only'>Close panel</span>
+                        <span className='sr-only'>Cerrar</span>
                         <XMarkIcon aria-hidden='true' className='h-6 w-6' />
                       </button>
                     </div>
@@ -67,83 +67,87 @@ export default function CartFav({ type, onClose }) {
 
                   <div className='mt-8'>
                     <div className='flow-root'>
-                      <ul role='list' className='-my-6 divide-y divide-gray-200'>
-                        {type === "favs"
-                          ? fav.map((product) => (
-                              <li key={product.id} className='flex py-6'>
-                                <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
-                                  <img
-                                    alt={product.name}
-                                    src={product.image}
-                                    className='h-full w-full object-cover object-center'
-                                  />
-                                </div>
-                                <div className='ml-4 flex flex-1 flex-col'>
-                                  <div>
-                                    <div className='flex justify-between text-base font-medium text-gray-900'>
-                                      <h3>
-                                        <a href={product.href}>{product.name}</a>
-                                      </h3>
-                                      <p className='ml-4'>{product.price}</p>
-                                    </div>
-                                    <p className='mt-1 text-sm text-gray-500'>
-                                      {product.color}
-                                    </p>
+                      {fav.length === 0 || cart.length === 0 ? (
+                        <p>Tu {type} esta vacio</p>
+                      ) : (
+                        <ul role='list' className='-my-6 divide-y divide-gray-200'>
+                          {type === "favs"
+                            ? fav.map((product) => (
+                                <li key={product.id} className='flex py-6'>
+                                  <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+                                    <img
+                                      alt={product.name}
+                                      src={product.image}
+                                      className='h-full w-full object-cover object-center'
+                                    />
                                   </div>
-                                  <div className='flex flex-1 items-end justify-between text-sm'>
-                                    <p className='text-gray-500'>
-                                      Qty {product.quantity}
-                                    </p>
-                                    <div className='flex'>
-                                      <button
-                                        type='button'
-                                        className='font-medium text-indigo-600 hover:text-indigo-500'
-                                      >
-                                        Remove
-                                      </button>
+                                  <div className='ml-4 flex flex-1 flex-col'>
+                                    <div>
+                                      <div className='flex justify-between text-base font-medium text-gray-900'>
+                                        <h3>
+                                          <a href={product.href}>{product.name}</a>
+                                        </h3>
+                                        <p className='ml-4'>{product.price}</p>
+                                      </div>
+                                      <p className='mt-1 text-sm text-gray-500'>
+                                        {product.color}
+                                      </p>
                                     </div>
-                                  </div>
-                                </div>
-                              </li>
-                            ))
-                          : cart.map((product) => (
-                              <li key={product.id} className='flex py-6'>
-                                <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
-                                  <img
-                                    alt={product.name}
-                                    src={product.image}
-                                    className='h-full w-full object-cover object-center'
-                                  />
-                                </div>
-                                <div className='ml-4 flex flex-1 flex-col'>
-                                  <div>
-                                    <div className='flex justify-between text-base font-medium text-gray-900'>
-                                      <h3>
-                                        <a href={product.href}>{product.name}</a>
-                                      </h3>
-                                      <p className='ml-4'>{product.price}</p>
-                                    </div>
-                                    <p className='mt-1 text-sm text-gray-500'>
-                                      {product.color}
-                                    </p>
-                                  </div>
-                                  <div className='flex flex-1 items-end justify-between text-sm'>
-                                    <p className='text-gray-500'>
-                                      Qty {product.quantity}
-                                    </p>
-                                    <div className='flex'>
-                                      <button
-                                        type='button'
-                                        className='font-medium text-indigo-600 hover:text-indigo-500'
-                                      >
-                                        Remove
-                                      </button>
+                                    <div className='flex flex-1 items-end justify-between text-sm'>
+                                      <p className='text-gray-500'>
+                                        Qty {product.quantity}
+                                      </p>
+                                      <div className='flex'>
+                                        <button
+                                          type='button'
+                                          className='font-medium text-indigo-600 hover:text-indigo-500'
+                                        >
+                                          Remove
+                                        </button>
+                                      </div>
                                     </div>
                                   </div>
-                                </div>
-                              </li>
-                            ))}
-                      </ul>
+                                </li>
+                              ))
+                            : cart.map((product) => (
+                                <li key={product.id} className='flex py-6'>
+                                  <div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+                                    <img
+                                      alt={product.name}
+                                      src={product.image}
+                                      className='h-full w-full object-cover object-center'
+                                    />
+                                  </div>
+                                  <div className='ml-4 flex flex-1 flex-col'>
+                                    <div>
+                                      <div className='flex justify-between text-base font-medium text-gray-900'>
+                                        <h3>
+                                          <a href={product.href}>{product.name}</a>
+                                        </h3>
+                                        <p className='ml-4'>{product.price}</p>
+                                      </div>
+                                      <p className='mt-1 text-sm text-gray-500'>
+                                        {product.color}
+                                      </p>
+                                    </div>
+                                    <div className='flex flex-1 items-end justify-between text-sm'>
+                                      <p className='text-gray-500'>
+                                        Qty {product.quantity}
+                                      </p>
+                                      <div className='flex'>
+                                        <button
+                                          type='button'
+                                          className='font-medium text-indigo-600 hover:text-indigo-500'
+                                        >
+                                          Remove
+                                        </button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </li>
+                              ))}
+                        </ul>
+                      )}
                     </div>
                   </div>
                 </div>
