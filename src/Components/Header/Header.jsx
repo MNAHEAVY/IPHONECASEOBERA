00719ec7fa54d70cdf7ad7885f63db9066cfce32
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2024-08-30 13:46:54 -0300

    fix add to cart

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -160,6 +160,7 @@ export default function Header() {
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-500/10'>
               <div className='space-y-2 py-6'>
+                <ProductsMenu />
                 {navigation.map((item) => (
                   <a
                     key={item.name}
