8f124f72f58d7cacc9723299341bac9de21cabff
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2026-03-09 12:59:38 -0300

    Fix conditional check for selectedVariant price

--- src/Views/ProductDetail/ProductDetail.jsx
+++ src/Views/ProductDetail/ProductDetail.jsx
@@ -391,7 +391,7 @@ const handleBuy = () => {
                   )}
                 </form>
 
-                {selectVariant.precio !== null && (
+                {selectedVariant.precio !== null && (
                   <button
                     className='mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed'
                     onClick={() => {
@@ -407,7 +407,7 @@ const handleBuy = () => {
                   </button>
                 )}
 
-                {selectVariant.precio !== null && (
+                {selectedVariant.precio !== null && (
                   <button
                     className='mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-black px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed'
                     onClick={() => {
